8a276b02b702e4248e59cd29a0908e79f5e00c65
Shrey <shrey.patidar99@gmail.com>
2023-12-29 23:22:11 +0530

    Initial Commit

---
+++ src/test/java/com/personalProjects/IPLDashboard/IplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalProjects.IPLDashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
